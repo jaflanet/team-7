7d5804acf885b63f86fc8413febddac43b305a08
I Dewa Gede Ngr Bramasta D. <bramastadarmawan5481@gmail.com>
2024-09-11 11:18:12 +0700

    Create Repository

---
+++ src/main/java/com/example/FinalProject/repository/PatronRepository.java
@@ -0,0 +1,11 @@
+package com.example.FinalProject.repository;
+
+import com.example.FinalProject.entity.PatronEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface PatronRepository extends JpaRepository<PatronEntity, Long> {
+    boolean existsByEmail(String email);
+}
